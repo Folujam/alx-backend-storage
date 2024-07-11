ad3cf32c57da0d0f06d1ef6cfdd04953f01bfdcf
folujam <olusway@gmail.com>
2024-07-11 15:29:10 +0100

    test all

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates new table users even if it exists.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
