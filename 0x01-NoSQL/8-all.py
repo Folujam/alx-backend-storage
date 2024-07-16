49b1c73faf3d9917c4b0f0880dadaa2e54684b4c
folujam <olusway@gmail.com>
2024-07-16 11:50:47 +0100

    test #1 -main added

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,3 +5,7 @@
 def list_all(mongo_collection):
     """lists all documents in collection"""
     return [document for document in mongo_collection.find()]
+
+if __name__ == "__main__":
+    """this is execusioner"""
+    list_all(mongo_collection)
