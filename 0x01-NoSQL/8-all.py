07ef3d7e5f77cd2f78d82ca84899e67686a1dc47
folujam <olusway@gmail.com>
2024-07-16 01:52:30 +0100

    t8 returns

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Py MongoDB Module"""
+
+
+def list_all(mongo_collection):
+    """lists all documents in collection"""
+    return [document for document in mongo_collection.find()]
