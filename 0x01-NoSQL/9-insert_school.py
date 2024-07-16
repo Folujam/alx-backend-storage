1b853c2f8ef83eecb02890f0d05ed0a3e04162d1
folujam <olusway@gmail.com>
2024-07-16 12:28:50 +0100

    test t9 #0

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Insert with python Module"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new doc in a collectn, returns new id"""
+    id = mongo_collection.insertOne(kwargs)
+    return id.inserted_id
+
+if __name__ == "__main__":
+    """the main execusioner"""
+    insert_school(mongo_collection, kwargs)
